36bad9265d67ba3e244d9f194bab57978b968b30
PCGUIMARAES <pessoal@pcguimaraes.com>
2020-03-15 13:27:18 -0300

    lista 4 completa

--- atividade-04-08.js
+++ atividade-04-08.js
@@ -7,8 +7,40 @@
  * Pedra ganha de Tesoura
  */
 var rs = require('readline-sync')
+var nomeA = rs.question('JOGADOR A Digite seu nome: ')
+var nomeB = rs.question('JOGADOR B Digite seu nome: ')
+var papel = 1
+var pedra = 2
+var tesoura = 3
+var jogo = true
+var jogoA
+var jogoB
 
+while(jogo){
+    jogoA = rs.questionInt(`Escolha uma das opcoes, ${nomeA}:\n[1] Papel\n[2]Pedra\n[3]Tesoura\n`, {hideEchoBack: true})
+    jogoB = rs.questionInt(`Escolha uma das opcoes, ${nomeB}:\n[1] Papel\n[2]Pedra\n[3]Tesoura\n`, {hideEchoBack: true})
+    if(jogoA == papel){
+        if(jogoB == papel){console.log('Deu empate!')}
+        else if(jogoB == pedra){console.log(nomeA + ' venceu!')}
+        else if(jogoB == tesoura){nomeB + 'venceu!'}
+        else{console.log(`A jogada do ${nomeB} eh ivalida!`)}
+    }
+    else if(jogoA == pedra){
+        if(jogoB == papel){console.log(nomeB + 'venceu!')}
+        else if(jogoB == pedra){console.log('Deu empate')}
+        else if(jogoB == tesoura){nomeA + 'venceu!'}
+        else{console.log(`A jogada do ${nomeB} eh ivalida!`)}
+    }
+    else if(jogoA == tesoura){
+        if(jogoB == papel){console.log(nomeA + 'venceu!')}
+        else if(jogoB == pedra){console.log(nomeB + 'venceu')}
+        else if(jogoB == tesoura){'Deu empate'}
+        else{console.log(`A jogada do ${nomeB} eh ivalida!`)}
+    }
+    else{
+        console.log(`A jogada do ${nomeA} eh ivalida!`)
+    }
 
-
-var password = rs.question('PASSWORD: ', {hideEchoBack: true})
- console.log(password)
+jogo = rs.keyInYN('Desejam jogar novamente? ')
+}
+console.log('Obrigado!')
